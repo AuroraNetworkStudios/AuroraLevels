46c602a00cda777809c209ecd50187b02544b210
Erik Szabó <erik1szabo1@gmail.com>
2024-06-03 09:35:41 +0200

    Release 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "gg.auroramc"
-version = "1.1.0-SNAPSHOT"
+version = "1.1.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 java.targetCompatibility = JavaVersion.VERSION_21
