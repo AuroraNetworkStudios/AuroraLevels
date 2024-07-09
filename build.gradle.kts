de8b212e40e74e563b3d412ebc1206b4c4ecb054
Erik Szabó <erik1szabo1@gmail.com>
2024-07-09 11:10:08 +0200

    Release 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "gg.auroramc"
-version = "1.3.3-SNAPSHOT"
+version = "1.4.0"
 
 java.sourceCompatibility = JavaVersion.VERSION_21
 java.targetCompatibility = JavaVersion.VERSION_21
@@ -37,7 +37,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-    compileOnly("gg.auroramc:Aurora:1.4.1-SNAPSHOT")
+    compileOnly("gg.auroramc:Aurora:1.4.1")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("dev.aurelium:auraskills-api-bukkit:2.0.7")
     compileOnly("io.lumine:Mythic-Dist:5.6.1")
