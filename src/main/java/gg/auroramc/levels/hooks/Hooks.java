a057b2cd04c5a8ba797f13f12aabbb9f021a0463
Erik Szabó <erik1szabo1@gmail.com>
2024-09-29 17:59:05 +0200

    Fix EcoSkills hook register

--- src/main/java/gg/auroramc/levels/hooks/Hooks.java
+++ src/main/java/gg/auroramc/levels/hooks/Hooks.java
@@ -1,6 +1,7 @@
 package gg.auroramc.levels.hooks;
 
 import gg.auroramc.levels.hooks.auraskills.AuraSkillsHook;
+import gg.auroramc.levels.hooks.ecoskills.EcoSkillsHook;
 import gg.auroramc.levels.hooks.luckperms.LuckPermsHook;
 import gg.auroramc.levels.hooks.mmolib.MMOLibHook;
 import gg.auroramc.levels.hooks.mythic.MythicHook;
@@ -10,7 +11,7 @@ import lombok.Getter;
 @Getter
 public enum Hooks {
     AURA_SKILLS(AuraSkillsHook.class, "AuraSkills"),
-    ECO_SKILLS(AuraSkillsHook.class, "EcoSkills"),
+    ECO_SKILLS(EcoSkillsHook.class, "EcoSkills"),
     LUCK_PERMS(LuckPermsHook.class, "LuckPerms"),
     MYTHIC_MOBS(MythicHook.class, "MythicMobs"),
     WORLD_GUARD(WorldguardHook.class, "WorldGuard"),
