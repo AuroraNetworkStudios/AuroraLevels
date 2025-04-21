dc8df87ca8734dfdb5208f77020944366cb62f60
Erik Szabó <erik1szabo1@gmail.com>
2025-04-21 22:32:57 +0200

    Update papermc repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ java.targetCompatibility = JavaVersion.VERSION_21
 repositories {
     mavenCentral()
     mavenLocal()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.auroramc.gg/releases/")
     maven("https://repo.aikar.co/content/groups/aikar/")
     maven("https://mvn.lumine.io/repository/maven-public/")
